95e4c3cf63c49b5e29c52f37911f832560279a96
Pete <123288439+DrPBaksh@users.noreply.github.com>
2025-03-11 11:14:06 +0000

    Update App.js with new routes for Alpine relocation theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import MountainList from './pages/MountainList';
+import Locations from './pages/Locations';
+import Resources from './pages/Resources';
 import './App.css';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mountains" element={<MountainList />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/resources" element={<Resources />} />
       </Routes>
     </Router>
   );
